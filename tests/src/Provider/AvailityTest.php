dd313105205fbb615edb587c03ce338be42d8a8f
Drew Hammond <drew@alphagenetica.com>
2018-02-20 18:25:04 -0500

    Code style fixes

--- tests/src/Provider/AvailityTest.php
+++ tests/src/Provider/AvailityTest.php
@@ -4,10 +4,11 @@ namespace Drewhammond\OAuth2\Client\Test\Provider;
 
 use PHPUnit\Framework\TestCase;
 
-class AvailityTest extends TestCase {
+class AvailityTest extends TestCase
+{
 
-
-
-
-
+    public function testInit()
+    {
+        $this->assertTrue(true);
+    }
 }
